804daa95333d90db1633e0759141769f9ae986e6
sauravvvpal <sauravpal2001@gmail.com>
2024-06-02 09:52:07 +0530

    server link updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,4 +20,6 @@ const store = configureStore({
 
 export default store;
 
-export const server = 'http://localhost:5000/api/v1';
+// export const server = 'http://localhost:5000/api/v1';
+
+export const server = 'https://coursesphere.onrender.com';
